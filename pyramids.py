be864d926e1659bfc45a52f9e6916f653ca71b04
matthewpaul <mspaul@udel.edu>
2015-10-30 10:11:20 -0400

    added reduce and expand methods

--- pyramids.py
+++ pyramids.py
@@ -24,12 +24,30 @@ def save_as_image(ar, pic):
 def norm(ar):
 	return 255.*np.absolute(ar)/np.max(ar)
 
-# PART 1 Function Definitions
+########################################### PART 1 Function Definitions ####################################################
 def convolve(image, kernel):
 	imgArray = np_from_image(image)
 	return norm(sg.convolve(imgArray, kernel))
 
+# Performs a gaussian convolution, and then reduces image size by half the width and height.
+def reduce(image):
+	width, height = Image.open(image).size
+	img = Image.fromarray(convolve(image, gaussian()).round().astype(np.uint8))
+	return img.resize((width / 2, height / 2))
+
+# Returns an image that is 2 times the height and width of the original image. 
+def expand(image):
+	width, height = Image.open(image).size
+	return Image.open(image).resize((width * 2, height * 2))
+
+#############################################################################################################################
+
+
 # Use the scipy convolve function to convolve the kernel around the image.
 # This sets up a basis for testing our convolve function
+singleDGaussian = [0.000229, 0.005977, 0.060598, 0.241732, 0.382928, 0.241732, 0.060598, 0.005977, 0.000229]
 
 save_as_image(convolve('img/portal.png', gaussian()), 'img/gportal.png')
+reduce('img/portal.png').save('img/rportal.png')
+expand('img/portal.png').save('img/bigportal.png')
+
